001f576abd9a8b197c57925517b3df535f0d3814
joooooker1100 <miladnazim10@gmail.com>
2024-10-17 02:41:22 +0330

    .

--- src/warehouse/controllers/warehouse.controller.ts
+++ src/warehouse/controllers/warehouse.controller.ts
@@ -3,23 +3,21 @@ import { WareHouseService } from "../services/warehouse.service";
 import { WareHouseModel } from "../model/warehouse.model";
 import { Ctx, MessagePattern, Payload, RmqContext } from "@nestjs/microservices";
 import { WareHouseInterface } from "../interfaces/warehouse.interface";
+import { RabitService } from "../../rabbit/rabbit.servise.ts/rabit.service";
 
 @Controller('warehouse')
 export class WareHouseController {
-    constructor(private readonly warehouseService: WareHouseService) {
+    constructor(private readonly warehouseService: WareHouseService,
+        private readonly rabbitService: RabitService
+    ) {
     }
     @MessagePattern({ event: 'product-get' })
     public async getProduct(@Payload() product: WareHouseInterface) {
         console.log("d", product)
         const sku = product.sku
         const getProduct = await this.warehouseService.getProduct(sku)
-        console.log(getProduct.qt)
-        if (getProduct.qt > 1) {
-            console.log("mojod")
-        } else {
-            console.log("na mojod")
-        }
-
+        console.log(getProduct)
+        await this.rabbitService.sendMessage('product-status', getProduct)
         return getProduct
 
 
